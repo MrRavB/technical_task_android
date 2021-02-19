bd3b303b5deeb4a07749e138db664800d7d7a102
RavB <berkowicz.r@gmail.com>
2021-02-19 15:56:52 +0100

    layout minor changes

--- app/src/main/java/com/example/lastbutnotleast/MainActivity.kt
+++ app/src/main/java/com/example/lastbutnotleast/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -29,10 +30,9 @@ import org.threeten.bp.LocalDateTime
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.materialIcon
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.gesture.longPressGestureFilter
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : AppCompatActivity() {
 
@@ -121,9 +121,17 @@ fun Users(usersViewModel: UsersViewModel) {
         Dialog(onDismissRequest = { usersViewModel.clearUserToCreate() }) {
             Box(modifier = Modifier.background(MaterialTheme.colors.background).padding(16.dp)) {
                 Column(horizontalAlignment = Alignment.End) {
-                    TextField(value = user.name, onValueChange = { usersViewModel.updateUserToCreateName(it) }, label = { Text("Name") })
+                    TextField(value = user.name,
+                        onValueChange = { usersViewModel.updateUserToCreateName(it) },
+                        label = { Text("Name") },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+                    )
                     Spacer(modifier = Modifier.padding(8.dp))
-                    TextField(value = user.email, onValueChange = { usersViewModel.updateUserToCreateEmail(it) }, label = { Text("Email") })
+                    TextField(value = user.email,
+                        onValueChange = { usersViewModel.updateUserToCreateEmail(it) },
+                        label = { Text("Email") },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+                    )
                     Spacer(modifier = Modifier.padding(8.dp))
                     Row(horizontalArrangement = Arrangement.End) {
                         Button(onClick = { usersViewModel.clearUserToCreate() }) { Text("Cancel") }
